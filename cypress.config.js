a21273655cabdf39135c7c694db4fc5317ded251
unknown <mrayferdinand@gmail.com>
2026-01-05 11:43:18 +0700

    First Commit

---
+++ cypress.config.js
@@ -0,0 +1,21 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  // Dengan Mochawesome
+  reporter: 'mochawesome',   // <--- Tambahkan di sini
+  reporterOptions: {         // <--- Opsi konfigurasi Mochawesome
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: true,
+    json: true
+  },
+  e2e: {
+    baseUrl: 'https://wordpress-1558210-6113754.cloudwaysapps.com/',
+    experimentalStudio: true,
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
